6902924c15c028455471494fe12738229b40ff31
Joshua Milano <milanoj925@gmail.com>
2026-08-10 22:42:21 +0100

    Removed metadata handling from Audio Engine

--- backend/AudioEngine.py
+++ backend/AudioEngine.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 from pathlib import Path
 import gc
 
-from backend.BackendHelpers import sanitise_data, get_primary_artist
+from backend.SongHelpers import SongMetadata, get_song_metadata
 
 
 # TODO: Upgrade engine to support up to 32bit audio
@@ -12,39 +12,6 @@ from backend.BackendHelpers import sanitise_data, get_primary_artist
 
 # This controls the framerate of the UI audio slider.
 slider_framerate = 60
-
-@dataclass
-class SongMetadata:
-    """Dataclass to store music metadata"""
-    title: str
-    artist: str
-    album: str
-    album_artist: str
-    duration_ms: int
-
-def get_song_metadata(file_path_str):
-    file_path = Path(file_path_str)
-    try:
-        audio = mutagen.File(file_path, easy=True)
-    except TypeError:
-        audio = mutagen.File(file_path)
-
-    if audio is None:
-        return None
-
-    raw_title = audio.get('title', [file_path.stem])[0]
-    raw_artist = get_primary_artist(audio.get('artist', ['Unknown Artist'])[0])
-    raw_album = audio.get('album', ['Unknown Album'])[0]
-    raw_album_artist = get_primary_artist((audio.get('albumartist') or audio.get('artist', ['Unknown Artist']))[0])
-    raw_duration = int(audio.info.length)
-    
-    return SongMetadata(
-        title = sanitise_data(raw_title),
-        artist = sanitise_data(raw_artist),
-        album = sanitise_data(raw_album),
-        album_artist = sanitise_data(raw_album_artist),
-        duration_ms = raw_duration * 1000
-    )
 
 # The Purpose of the StreamProxy is to hijack the connection between the AudioEngine and the miniaudio C library
 class StreamProxy:
--- backend/SongHelpers.py
+++ backend/SongHelpers.py
@@ -1,1 +1,39 @@
 from dataclasses import dataclass
+from pathlib import Path
+import mutagen
+
+from backend.BackendHelpers import get_primary_artist
+
+@dataclass
+class SongMetadata:
+    """Dataclass to store music metadata"""
+    title: str
+    artist: str
+    album: str
+    album_artist: str
+    duration_ms: int
+
+
+def get_song_metadata(file_path_str):
+    file_path = Path(file_path_str)
+    try:
+        audio = mutagen.File(file_path, easy=True)
+    except TypeError:
+        audio = mutagen.File(file_path)
+
+    if audio is None:
+        return None
+
+    raw_title = audio.get('title', [file_path.stem])[0]
+    raw_artist = get_primary_artist(audio.get('artist', ['Unknown Artist'])[0])
+    raw_album = audio.get('album', ['Unknown Album'])[0]
+    raw_album_artist = get_primary_artist((audio.get('albumartist') or audio.get('artist', ['Unknown Artist']))[0])
+    raw_duration = int(audio.info.length)
+    
+    return SongMetadata(
+        title = raw_title,
+        artist = raw_artist,
+        album = raw_album,
+        album_artist = raw_album_artist,
+        duration_ms = raw_duration * 1000
+    )
